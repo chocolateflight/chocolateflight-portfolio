85fa05d69a50614b900ec22b2a9ec3ebc508f8f0
Marc <67051612+chocolateflight@users.noreply.github.com>
2023-02-04 11:10:15 +0100

    initial boilerplate

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,23 @@
-import './globals.css'
+import './globals.css';
+import { Montserrat } from '@next/font/google';
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+const montserrat = Montserrat({
+  weight: ['400', '500', '600', '700'],
+  subsets: ['latin'],
+  variable: "--font-montserrat",
+});
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
+    <html lang='en'>
       <head />
-      <body>{children}</body>
+
+      <body className={`${montserrat.className}`}>
+        <nav>
+          <p>Nav</p>
+        </nav>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
