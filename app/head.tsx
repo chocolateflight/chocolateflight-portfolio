140297b63e5a03c94d6d17f0b6ee022963ede840
Marc <67051612+chocolateflight@users.noreply.github.com>
2023-02-24 13:29:40 +0100

    updated meta tags

--- app/head.tsx
+++ app/head.tsx
@@ -2,10 +2,20 @@ export default function Head() {
   return (
     <>
       <title>Marc Hostettler</title>
-      <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="A Portfolio Website" />
-      <meta name="author" content="Marc Hostettler" />
-      <link rel="icon" href="/favicon.jpg" />
+      <meta content='width=device-width, initial-scale=1' name='viewport' />
+      <meta name='description' content='Marc Hostettler - A Portfolio Website' />
+      <meta name='author' content='Marc Hostettler' />
+      <meta name='keywords' content='Marc Hostettler, Portfolio, Web Developer' />
+      <meta charSet='utf-8' />
+      <meta property='og:title' content='Marc Hostettler - A Portfolio Website' />
+      <meta
+        property='og:description'
+        content='The portfolio website of Marc Hostettler. An aspiring Web Developer based in Switzerland.'
+      />
+      <meta property='og:type' content='website' />
+      <meta property='og:url' content='https://chocolateflight.vercel.app/' />
+      <meta property='og:site_name' content='Marc Hostettler - A Portfolio Website' />
+      <link rel='icon' href='/favicon.jpg' />
     </>
-  )
+  );
 }
